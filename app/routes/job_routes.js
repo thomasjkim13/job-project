f68b83603f8fad790551cefa36dc70d05914ae76
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-04-21 16:25:17 -0400

    Update job routes GET

--- app/routes/job_routes.js
+++ app/routes/job_routes.js
@@ -1,38 +1,54 @@
+// Express docs: http://expressjs.com/en/api.html
 const express = require('express')
-// jsonwebtoken docs: https://github.com/auth0/node-jsonwebtoken
-const crypto = require('crypto')
 // Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
-// bcrypt docs: https://github.com/kelektiv/node.bcrypt.js
-// bcrypt is a password-hashing function
-const bcrypt = require('bcrypt')
 
-// see above for explanation of "salting", 10 rounds is recommended
-const bcryptSaltRounds = 10
+// pull in Mongoose model for jobs
+const Jobs = require('../models/jobs')
 
-// pull in error types and the logic to handle them and set status codes
-const errors = require('../../lib/custom_errors')
+// this is a collection of methods that help us detect situations when we need
+// to throw a custom error
+const customErrors = require('../../lib/custom_errors')
 
-const BadParamsError = errors.BadParamsError
-const BadCredentialsError = errors.BadCredentialsError
+// we'll use this function to send 404 when non-existant document is requested
+const handle404 = customErrors.handle404
+// we'll use this function to send 401 when a user tries to modify a resource
+// that's owned by someone else
+const requireOwnership = customErrors.requireOwnership
 
-const Job = require('../models/jobs')
-const jobs = require('../models/jobs')
-
+// this is middleware that will remove blank fields from `req.body`, e.g.
+// { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
+const removeBlanks = require('../../lib/remove_blank_fields')
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
-// it will also set `res.user`
+// it will also set `req.user`
 const requireToken = passport.authenticate('bearer', { session: false })
 
 // instantiate a router (mini app that only handles routes)
 const router = express.Router()
+
+// INDEX
+// GET /jobs
+router.get('/jobs', requireToken, (req, res, next) => {
+    Jobs.find()
+      .then(jobs => {
+        // `examples` will be an array of Mongoose documents
+        // we want to convert each one to a POJO, so we use `.map` to
+        // apply `.toObject` to each one
+        return jobs.map(job => job.toObject())
+      })
+      // respond with status 200 and JSON of the examples
+      .then(jobs => res.status(200).json({ jobs: jobs }))
+      // if an error occurs, pass it to the handler
+      .catch(next)
+  })
 
 // Create job
 // POST
 router.post('/jobs', requireToken, (req, res, next) => {
     // set owner of new example to be current user
     req.body.jobs.owner = req.user.id
-    jobs.create(req.body.jobs)
+    Jobs.create(req.body.jobs)
       .then(job => {
         res.status(201).json({ job: job.toObject() })
       })
