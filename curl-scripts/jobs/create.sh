5108272a0afdda3b48741dc6d378372918b74e35
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-04-21 14:21:58 -0400

    Add job POST curl-script

---
+++ curl-scripts/jobs/create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/create"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "jobs": {
+      "title": "'"${TITLE}"'",
+      "company": "'"${COMPANY}"'",
+      "experience_level": "'"${LEVEL}"'"
+    }
+  }'
+
+echo
